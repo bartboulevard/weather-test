a360baeefe9a31dc57607d5f087fee32a5e8a34d
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2024-02-20 11:30:50 +0200

    Made an API call for OpenWeatherMap to display name, latitude and longitude of city. Created test suites and a main view

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,67 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor, within} from '@testing-library/react';
 import App from './App';
+import Search from "./components/Search";
+import {createMockServer} from "./createMockServer";
+import userEvent from "@testing-library/user-event";
+import {act} from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Weather application tests', () => {
+  let server;
+  beforeEach(() => {
+    server = createMockServer()
+  })
+  afterEach(() => {
+    server.shutdown()
+  })
+
+  it('renders weather application title', function () {
+    render(<App />);
+    const linkElement = screen.getByText(/Weather Application/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('shows city search results', async () => {
+    render(<Search />);
+
+    const input = screen.getByTestId('search-input')
+    userEvent.type(input, 'Melbourne')
+
+    const button = screen.getByTestId('search-button')
+    userEvent.click(button)
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+    expect(screen.getByText)
+  });
+
+  it('shows city search result details', async () => {
+    render(<Search />);
+
+    const input = screen.getByTestId('search-input')
+    userEvent.type(input, 'Melbourne')
+
+    const button = screen.getByTestId('search-button')
+    userEvent.click(button)
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+    expect(screen.getByText(/Melbourne, -37.8142176, 144.9631608/i)).toBeInTheDocument()
+  });
+
+  it('add search result to my weather list', async () => {
+    render(<Search />);
+
+    const input = screen.getByTestId('search-input')
+    userEvent.type(input, 'Melbourne')
+
+    const button = screen.getByTestId('search-button')
+    userEvent.click(button)
+
+    await waitFor(() => expect (screen.getAllByText(/Melbourne/i).length).toEqual(5))
+
+    const selected = screen.getAllByText(/Melbourne/i) [3]
+    act(() => {
+    userEvent.click(selected)
+    })
+
+    expect(within(screen.getByTestId('my-weather-list')).getByText(/Melbourne/i)).toBeInTheDocument()
+  });
 });
---
+++ src/components/Search.js
@@ -0,0 +1,59 @@
+import {useState} from "react";
+
+import {createMockServer} from '../createMockServer';
+
+if(process.env.NODE_ENV === 'development') {
+    createMockServer()
+}
+
+function Search() {
+    const [query, setQuery] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+    const [selected, setSelected] = useState([])
+
+    const inputChangeHandler = (event) => {
+        setQuery(event.target.value)
+    }
+
+    const buttonClickHandler = () => {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`)
+            .then((result) => {
+                return result.json()
+            })
+            .then((cities) => {
+                setSearchResults(cities.map((city) => ({
+                    name: city.name,
+                    country: city.country,
+                    lat: city.lat,
+                    lon: city.lon
+                })))
+            })
+    }
+
+    const selectCity = (city) => {
+        setSelected([city, ...selected])
+    }
+
+    return (
+        <div className="App">
+            <input type="text" data-testid="search-input" onChange={inputChangeHandler}/>
+            <button data-testid="search-button" onClick={buttonClickHandler}>Search</button>
+
+            <div data-testid="search-results">
+                {searchResults.map((city) => <div key={`${city.lat}-${city.lon}`} onClick={() => selectCity(city)}>{city.name}, {city.lat}, {city.lon}
+                    </div>
+                )}
+            </div>
+
+            <div data-testid="my-weather-list">
+                {selected && selected.map((city) => <div
+                        key={`${city.lat}-${city.lon}`}>
+                        {city.name}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Search;
